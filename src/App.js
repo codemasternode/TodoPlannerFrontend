39bc00bd4f31b29bc46cb20046dc8227495325c7
javaxd123 <marcinwarzybok@outlook.com>
2018-05-06 20:42:44 +0200

    Basic root

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './css/App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;

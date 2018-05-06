99f8913d813e31cf2aaec33abecc69952778fa16
javaxd123 <marcinwarzybok@outlook.com>
2018-05-06 21:28:29 +0200

    start with styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,8 +5,14 @@ import CssModules from 'react-css-modules'
 class Nav extends React.Component {
     render() {
         return (
-            <div>
-                Nav
+            <div className="nav">
+                <div>
+                    <h2>TodoPlanner</h2>
+                </div>
+                <div className="box">
+                    <div>Logowanie</div>
+                    <div>Rejestracja</div>
+                </div>
             </div>
         )
     }

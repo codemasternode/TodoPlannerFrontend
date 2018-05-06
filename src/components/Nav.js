ffe18fa070c1f37cd1b511d2c020afcd89e8e23f
javaxd123 <marcinwarzybok@outlook.com>
2018-05-06 23:10:44 +0200

    after set background nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import navStyles from '../css/nav.css'
 import CssModules from 'react-css-modules'
+import { Link } from 'react-router-dom'
 
 class Nav extends React.Component {
     render() {
         return (
-            <div className="nav">
+            <div className="nav primary">
                 <div>
                     <h2>TodoPlanner</h2>
                 </div>
                 <div className="box">
-                    <div>Logowanie</div>
-                    <div>Rejestracja</div>
+                    <div>
+                        Logowanie
+                    </div>
+                    <div>
+                        <Link to="/register">
+                            Rejestracja
+                        </Link>
+                    </div>
                 </div>
             </div>
         )

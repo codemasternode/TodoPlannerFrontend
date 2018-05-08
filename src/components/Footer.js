3ecc4146eb8a8e3f5c66c1699d651e25c1339feb
javaxd123 <marcinwarzybok@outlook.com>
2018-05-08 10:13:50 +0200

    Start styling home, finished footer and vav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,29 @@ class Footer extends React.Component {
             <div className="footer primary">
                 <div className="container">
                     <div className="col">
-                        marcinwarzybok@outlook.com <br/>
+                        marcinwarzybok@outlook.com <br />
                         Made by Marcin Warzybok<br />
-                        20 listopada 2000<br/>
+                        20 listopada 2000<br />
                         tel.673 334 353<br />
                     </div>
                     <div className="col">
 
+                        <object type="image/svg+xml" data={require('../assets/icons/if_facebook_2308066.svg')}>
+                            <img src={require('../assets/icons/if_facebook_2308066.png')} />
+                        </object>
+                        <object type="image/svg+xml" data={require('../assets/icons/if_github_2308116.svg')}>
+                            <img src={require('../assets/icons/if_github_2308116.png')} />
+                        </object>
+                        <object type="image/svg+xml" data={require('../assets/icons/if_google-plus_2308064.svg')}>
+                            <img src={require('../assets/icons/if_google-plus_2308064.png')} />
+                        </object>
+                        <object type="image/svg+xml" data={require('../assets/icons/if_email_2308112.svg')}>
+                            <img src={require('../assets/icons/if_email_2308112.png')} />
+                        </object>
+
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,16 @@ import CssModules from 'react-css-modules'
 class Home extends React.Component {
     render() {
         return (
-            <div>
-                Home
+            <div className="container">
+                <div className="image-wrapper">
+                    <img src={require('../assets/static images/planowanie-spadku-05.jpg')} />
+                </div>
+                <div className="col">
+                    <h2>Czy wiesz że większość milarderów planuje swój dzień?</h2>
+                    <p>Ty też możesz dołączyć do ich niewielkiego grona i zaplanować swój dzień, naukowcy udowodnili że 80% osób które planują swój dzień dokładnie nie ma problemu z prokrastynacją</p>
+                </div>
             </div>
-        )
+                )
     }
 }
 

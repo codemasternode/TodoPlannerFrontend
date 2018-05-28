882c22b26e9c59e483cf38b1f72f91731c9ef115
javaxd123 <marcinwarzybok@outlook.com>
2018-05-28 22:59:02 +0200

    after routes

--- src/components/stack/SubChild.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SubChild = props => {
-    return <div>SubChild</div>
-}
-
-export default SubChild
--- src/components/stack/SubChild2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SubChild2 = props => {
-    return <div>SubChild</div>
-}
-
-export default SubChild2

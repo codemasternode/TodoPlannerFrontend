99493ee6fa54cd4316c2597fe8f9f6c61e008493
javaxd123 <marcinwarzybok@outlook.com>
2018-05-31 19:29:35 +0200

    created DateFormat

--- src/components/helpers/DateFormat.js
+++ src/components/helpers/DateFormat.js
@@ -5,14 +5,28 @@ class DateFormat {
         this.year = year
     }
 
-    increamentDays(inc, date) {
-        const dayInMonth = dayInMonth(date.year, date.month)
-        return new DateFormat()
+    increamentDays(inc, { day, month, year }) {
+        this.month = month
+        this.year = year
+        this.day = day
+        const dayOfMonth = dayInMonth(year, month)
+        if (day + inc > dayOfMonth) {
+            let roznica = dayOfMonth - day
+            inc = inc - roznica - 1
+            this.day = inc
+            this.month += 1
+            if (this.month > 11) {
+                this.month = 0
+                this.year += 1
+            }
+        }
+        return new DateFormat(this.day, this.month, this.year)
     }
 
+
+
 }
-
-
 function dayInMonth(year, month) {
     return new Date(year, month, 0).getDate();
 }
+export default DateFormat
--- src/components/todosComponents/DayTodos.js
+++ src/components/todosComponents/DayTodos.js
@@ -7,11 +7,25 @@ import DateFormat from '../helpers/DateFormat'
 class DayTodos extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            nowTime: new DateFormat(new Date().getDate(), new Date().getMonth(), new Date().getFullYear())
+        }
     }
     componentDidMount() {
         this.props.fetchDayTodos()
+        const day = this.state.nowTime.day
+        const month = this.state.nowTime.month
+        const year = this.state.nowTime.year
+        const addTime = new DateFormat()
+        addTime.increamentDays(6, { day, month, year })
+        console
+        this.setState({
+            addTime: addTime,
+            nowTime: new DateFormat(new Date().getDate(), new Date().getMonth(), new Date().getFullYear())
+        })
     }
     render() {
+        console.log(this.state)
         return <div>Day Plannerasdasd</div>
     }
 }
